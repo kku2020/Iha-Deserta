a591dfae149ae24f51e0e7679d594c27c49f4adf
kku2020 <kkurodrigues1996@gmail.com>
2021-02-25 18:51:34 -0300

    Exercício de Advinhação

---
+++ ilhadeserta.py
@@ -0,0 +1,5 @@
+item1 = input("Informe o primeiro item que voce levaria para uma ilha deserta.")
+item2 = input("Informe o segundo item que voce levaria para uma ilha deserta.")
+item3 = input("Informe o terceiro item que voce levaria para uma ilha deserta.")
+
+print (f"Você levaria para uma ilha deserta um(a),{item1},{item2} e{item3}")
